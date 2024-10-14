8372be1d004fa9053bc49d0b408eec4fbcdab6c8
cl9bix <yuraklyap1103@gmail.com>
2024-10-14 21:06:02 +0200

    adada

--- hazik/eljarasok/alap_3.py
+++ hazik/eljarasok/alap_3.py
@@ -3,9 +3,10 @@ def create_list():
 
 
 def fel_2(lst):
-    lst.sort(reverse=True)
+    masolat = lst.copy()
+    masolat.sort(reverse=True)
     szam = 0
-    for i in lst:
+    for i in masolat:
         if i % 2 == 0:
             szam += i
             break
@@ -13,9 +14,10 @@ def fel_2(lst):
 
 
 def fel_3(lst):
-    lst.sort()
+    masolat = lst.copy()
+    masolat.sort()
     szam = 0
-    for i in lst:
+    for i in masolat:
         if i % 2 == 0:
             szam += i
             break
@@ -24,26 +26,52 @@ def fel_3(lst):
 
 def is_prim(lst):
     van = False
-    for i in range(1, len(lst)):
+    for i in range(len(lst)):
         db_oszto = 0
-
-        for j in range(1, i + 1):
+        for j in range(1, lst[i] + 1):
             if lst[i] % j == 0:
                 db_oszto += 1
-        # if db_oszto == 2:
-        #     van = True
-        #     break
-    # if van:
-    #     return 'Van'
-    # else:
-    #     return 'Nincs'
+        if db_oszto == 2:
+            van = True
+    if van:
+        return 'Igen'
+    else:
+        return 'Nem'
+
+
+def fel_5(lst):
+    idx = 1
+    elso = 0
+    for i in lst:
+        if i % 2 == 0:
+            elso = i
+            break
+        idx += 1
+    return elso, idx
+
+
+def fel_6(lst):
+    osszeg = 0
+    for i in lst:
+        if i % 2 == 0:
+            osszeg += i
+    return osszeg
+
+
+def is_first_bigger_than_last(lst):
+    return 'Igen' if lst[0] > lst[-1] else 'Nem'
 
 
 def main():
     lst = create_list()
     print(f'2.feladat: {fel_2(lst)}')
     print(f'3.feladat: {fel_3(lst)}')
-    print(f'4.feladat: {is_prim(lst)}')
+    # print(f'4.feladat: {is_prim(lst)}')
+    print('4.feladat:', is_prim(lst))
+    print(f'5.feladat: Elso szam: {fel_5(lst)[0]} Indexje: {fel_5(lst)[1]}')
+    print(f'6.feladat: Paros szamok osszege:', fel_6(lst))
+    print('7.feladat:', fel_6(lst) / len(lst))
+    print(f'8.feladat:',is_first_bigger_than_last(lst) )
 
 
 if __name__ == '__main__':

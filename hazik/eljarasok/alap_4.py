13311d6f92e13c54ab5659b64544ce480856e954
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-10-17 12:31:38 +0200

    ашч

--- hazik/eljarasok/alap_4.py
+++ hazik/eljarasok/alap_4.py
@@ -2,8 +2,8 @@ import random
 
 
 def create_list():
-    return [random.randint(-50,50) for _ in range(10)]
-    # return [6,1,3,2,8,11,5,4,7,9,10]
+    # return [random.randint(-50,50) for _ in range(10)]
+    return [6,1,3,2,8,11,5,4,7,9,10]
 
 def num_sum(lst):
     osszeg = 0
@@ -55,7 +55,23 @@ def is_same_num(lst):
     else:
         return "Nincs"
 
+def equal_to_zero(lst):
+    van = False
+    for i in range(len(lst)-1):
+        if lst[i] + lst[i+1] == 0:
+            van = True
+    if van:
+        return "Van"
+    else:
+        return 'Nincs'
 
+
+def fel_10(lst):
+    db = 0
+    for i in range(1,len(lst) - 1):
+        if lst[i-1] > lst[i] and lst[i+1] > lst[i]:
+            db += 1
+    return db
 def main():
     lst = create_list()
     print('Lista')
@@ -63,8 +79,27 @@ def main():
         print(i,end=' ')
     print()
 
-    print('1.feladat:')
+    print('2.feladat:')
+    print(num_sum(lst))
+    print('3.feladat:')
+    print(count_positive_n(lst))
+    print('4.feladat:')
+    print(not_negative_atlag(lst))
+    print('5.feladat:')
     print(get_smallest_num(lst))
+    print('6.feladat:')
+    print(get_sec_biggest_num(lst))
+    print('7.feladat:')
+    print(sort_num_by_high(lst))
+    print('8.feladat:')
+    print(is_same_num(lst))
+    print('9.feladat:')
+    print(equal_to_zero(lst))
+    print('10.feladat:')
+    print(fel_10(lst))
+
+
+
 
 
 if __name__ == '__main__':
